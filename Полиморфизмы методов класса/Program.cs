c7752b363d97ab4987df4677e6451d1617b20191
quaswexexort1 <coldzerokot@gmail.com>
2024-12-27 11:23:27 +0200

    Update Program.cs

--- Полиморфизмы методов класса/Program.cs
+++ Полиморфизмы методов класса/Program.cs
@@ -1,4 +1,12 @@
-﻿using Полиморфизмы_методов_класса;
+using Полиморфизмы_методов_класса;
+using static Полиморфизмы_методов_класса.TelevizorSuper;
 
-televizor televizor = new televizor();
-Console.WriteLine(televizor);
+
+        televizor tv1 =FactoryTV.CreateTV("Samsung", 55, 20, "Корея");
+        Console.WriteLine(tv1);
+        televizor tv2 = FactoryTV.CreateTV("Sony", 65, 25, "Япония");
+        Console.WriteLine(tv2);
+        televizor tv3 = FactoryTV.CreateTV("LG", 40, 15, "Китай");
+        Console.WriteLine(tv3);
+        televizor tv4 = FactoryTV.CreateTV("Xiaomi", 50, 30, "Сингапур");
+        Console.WriteLine(tv4);
