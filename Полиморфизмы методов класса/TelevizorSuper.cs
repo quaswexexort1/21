2694d2f50ce98cd2f20159beab442bd7dce61813
quaswexexort1 <coldzerokot@gmail.com>
2024-12-27 11:23:42 +0200

    Update TelevizorSuper.cs

--- Полиморфизмы методов класса/TelevizorSuper.cs
+++ Полиморфизмы методов класса/TelevizorSuper.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,8 +6,46 @@ using System.Threading.Tasks;
 
 namespace Полиморфизмы_методов_класса
 {
-    internal class TelevizorSuper
+    internal class TelevizorSuper:televizor
     {
-        private int dwdadas;
+        public class ЯпонскийТелевизор : televizor
+        {
+            public override double РассчитатьQp()
+            {
+                return 2 * РассчитатьQp(); // Qp равен 2 * Q для японских телевизоров
+            }
+        }
+
+        public class АзиатскийТелевизор:televizor
+        {
+            public override double РассчитатьQp()
+            {
+                return 1.5 * РассчитатьQp(); // Qp равен 1.5 * Q для азиатских телевизоров
+            }
+        }
+
+        public class ОбычныйТелевизор:televizor
+        {
+            // Ничего не переопределяет
+        }
+
+
+        public static class FactoryTV
+        {
+            public static televizor CreateTV(string фирма, double диагональ, double мощность, string страна)
+            {
+                switch (страна.ToLower())
+                {
+                    case "Япония": return new ЯпонскийТелевизор { Фирма = фирма, Диагональ = диагональ, МощностьЗвука = мощность, СтранаПроизводитель = страна };
+                    case "Сингапур":
+                    case "Корея":
+                        return new АзиатскийТелевизор { Фирма = фирма, Диагональ = диагональ, МощностьЗвука = мощность, СтранаПроизводитель = страна };
+                    default:
+                        return new ОбычныйТелевизор { Фирма = фирма, Диагональ = диагональ, МощностьЗвука = мощность, СтранаПроизводитель = страна };
+                }
+            }
+        }
     }
+
+    
 }
